973a7ce95c28f7f5260c9294c05848f00321c6ec
Mingriweiji-github <18046506190@163.com>
2016-03-07 14:35:09 +0800

    Initial Commit

---
+++ SSZipArchive-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSZipArchive-test
+//
+//  Created by Mr.Li on 16/3/7.
+//  Copyright © 2016年 Mr.Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SSZipArchive-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SSZipArchive-test
+//
+//  Created by Mr.Li on 16/3/7.
+//  Copyright © 2016年 Mr.Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
